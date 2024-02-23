942f8b367503f0bf198cd5db8ae0ba62f5c9f17b
Samuel Veras <samuel.bernardo.veras@gmail.com>
2024-02-23 13:43:16 -0300

    IntArray init

--- IntArray/main.c
+++ IntArray/main.c
@@ -7,3 +7,20 @@ typedef struct IntArray {
     size_t max_size;
     size_t used;
 } IntArray;
+
+// Retorna pointer para array vazio com tamanho especificado
+IntArray* int_array_init(size_t initial_size) {
+    IntArray* array = malloc(sizeof(IntArray));
+
+    if (array == NULL)
+        perror("Erro em inicialização de array struct."), exit(1);
+
+    array->max_size = initial_size;
+    array->used = 0;
+    array->data = malloc(initial_size * sizeof(int));
+
+    if (array->data == NULL)
+        perror("Erro em inicialização de array."), exit(1);
+
+    return array;
+}
