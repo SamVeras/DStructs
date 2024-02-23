6fb812bbba71793015a74d6669e50228a77f1c1a
Samuel Veras <samuel.bernardo.veras@gmail.com>
2024-02-23 13:44:00 -0300

    Funções principais

--- IntArray/main.c
+++ IntArray/main.c
@@ -24,3 +24,50 @@ IntArray* int_array_init(size_t initial_size) {
 
     return array;
 }
+
+// Destroy array e todos os dados
+void int_array_destroy(IntArray* array) {
+    free(array->data);
+    free(array);
+}
+
+// Empurra valor para o final do array, mudando o tamanho do array se necessário
+void int_array_push(IntArray* array, int num) {
+    if (array->used == array->max_size) {
+        array->data = realloc(array->data, sizeof(int) * array->max_size * 2);
+        array->max_size *= 2;
+    }
+
+    array->data[array->used] = num;
+    array->used++;
+}
+
+// Remove e retorna o índice especificado
+int int_array_remove(IntArray* array, unsigned int index) {
+    if (index > array->used - 1)
+        perror("Erro em remove fora de índice"), exit(1);
+
+    int num = array->data[index];
+
+    for (int i = index; i < array->used - 1; i++)
+        array->data[i] = array->data[i + 1];
+
+    array->used--;
+
+    return num;
+};
+
+// Remove e retorna o último item do array
+int int_array_pop(IntArray* array) {
+    if (array->used == 0)
+        perror("Erro em pop lista vazia"), exit(1);
+    return array->data[--array->used];
+};
+
+const int int_array_get(IntArray* array, unsigned int index) {
+    if (index < array->used - 1)
+        perror("Erro em get fora de índice"), exit(1);
+
+    return array->data[index];
+}
+
